22c6a0211935bbebdc6fd3afe01b531cecd0897d
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-03-18 08:35:40 -0300

    feat: update global styles and create titulos page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1 className="titulo">Hello World!!</h1>
+      <h1 className={styles.titulo}>Hello World!!</h1>
     </div>
   );
 }
